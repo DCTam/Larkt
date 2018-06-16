2fe2593216c79a66475ad190ec3b0eecb068460b
Daniel Tam <ctam.daniel@gmail.com>
2018-06-16 00:02:07 -0700

    Added mock server

---
+++ LoginService/server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/health-check', (req, res) => {
+	res.send({
+		data: {
+			status: 'OK'
+		}
+	});
+});
+
+app.post('/login', (req, res) => {
+	res.send({
+		data: {
+			success: true
+		}
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server running on port 3000');
+});
